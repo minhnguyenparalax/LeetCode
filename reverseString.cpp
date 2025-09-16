e24331ba30ce8bfe9b06fd8274b34fa476a0bdca
Minh Nguyen <nguyenquang123uvb@gmail.com>
2025-09-16 23:25:40 +0700

    .

--- reverseString.cpp
+++ reverseString.cpp
@@ -1,3 +1,9 @@
+#include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
+
+
 class Solution {
 public:
     void reverseString(vector<char>& s) {
@@ -11,3 +17,35 @@ public:
         }
     }
 };
+
+void input_s(vector<char> &s)
+{
+    cout<<"Nhap ki tu: "<<endl;
+    int i = 0;
+    char char_stop = '.';
+    char tmp;
+    while(true)
+    {
+        cout<<"s["<<i<<"] = ";
+        cin.get(tmp);
+        cin.ignore();
+        if(tmp == '.')
+            break;
+        s.push_back(tmp);
+        i++;
+    }
+}
+int main()
+{
+    Solution slt;
+    vector<char> s;
+    input_s(s);
+    slt.reverseString(s);
+
+    cout<<"Chuoi sau khi dao nguoc la: "<<endl;
+    for(char c:s)
+    {
+        cout<<c;
+    }
+    
+}
