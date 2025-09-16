6f1539366b151931733198a5ee57d42899d31ca3
Minh Nguyen <nguyenquang123uvb@gmail.com>
2025-09-16 23:09:36 +0700

    .

--- 6.longestCommonPrefix.cpp
+++ 6.longestCommonPrefix.cpp
@@ -8,6 +8,7 @@ public:
     string longestCommonPrefix(vector<string>& strs) {
         if (strs.size() == 0) return "";
         string prefix = strs[0];
+        
         for (int i = 1; i < strs.size(); i++)
             while (strs[i].find(prefix) != 0) {
                 prefix = prefix.substr(0, prefix.length() - 1);
