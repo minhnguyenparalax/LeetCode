347648fc973e95b3bf4ba687dd29099ec6dce3c5
Minh Nguyen <nguyenquang123uvb@gmail.com>
2025-09-19 22:39:29 +0700

    .

--- 125.isValidPalindrome.cpp
+++ 125.isValidPalindrome.cpp
@@ -1,3 +1,22 @@
-//Palindrome is a text Which can read same forward and backward
-//after remove white space and convert uppercase to lowercase
-//Example:
+// //Palindrome is a text Which can read same forward and backward
+// //after remove white space and convert uppercase to lowercase
+// //Example: Input: s = "A man, a plan, a canal: Panama"
+// Output: true
+// Explanation: "amanaplanacanalpanama" is a palindrome.
+
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string newStr = "";
+        for (char i : s) {
+            if (isalnum(i)) {
+                newStr += tolower(i);
+            }
+        }
+        int l = 0, r = newStr.size() - 1;
+        while (l < r) {
+            if (newStr[l++] != newStr[r--]) return false;
+        }
+        return true;
+    }
+};
