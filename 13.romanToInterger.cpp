5babe55e0275a450c0e06db334f379ba4aa8f3c1
Minh Nguyen <nguyenquang123uvb@gmail.com>
2025-09-23 23:12:08 +0700

    .

--- 13.romanToInterger.cpp
+++ 13.romanToInterger.cpp
@@ -15,3 +15,30 @@
 // I can be placed before V (5) and X (10) to make 4 and 9. 
 // X can be placed before L (50) and C (100) to make 40 and 90. 
 // C can be placed before D (500) and M (1000) to make 400 and 900.
+
+#include <unordered_map>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    int romanToInt(string s) {
+        unordered_map<char, int> roman = {
+            {'I',1}, {'V',5}, {'X',10}, {'L',50},
+            {'C',100}, {'D',500}, {'M',1000}
+        };
+        
+        int result = 0;
+        for (int i = 0; i < s.size(); ++i) {
+            int curr = roman[s[i]];
+            int next = (i+1 < s.size()) ? roman[s[i+1]] : 0;
+            
+            if (curr < next) {
+                result -= curr;
+            } else {
+                result += curr;
+            }
+        }
+        return result;
+    }
+};
